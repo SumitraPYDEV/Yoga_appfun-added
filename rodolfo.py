70d5c675158895342f6d4d3b7beaceb6835cd0f0
sumitra <sumitrasr.py>
2023-01-24 01:25:37 -0500

    this is yoga app! name added.

---
+++ rodolfo.py
@@ -0,0 +1,8 @@
+""" this is the new app which would trigger herbeat of a person doing yoga"""
+
+def yoga1(name):
+    # name = "digital"
+    return name
+new_var = yoga1("rodolfo")
+print(new_var)
+
